55c74f162f049f02dddcf678b00ff32e458dd6e9
Ishaan Singh <ishaananuraag@gmail.com>
2019-05-11 18:49:28 +0530

    Implemented basic content opening

--- code/public/main.js
+++ code/public/main.js
@@ -37,15 +37,44 @@ require(['marionette','jquery','IUI'],function(Marionette,$,IUI){
 	window.$=$;
 	window.IUI=IUI;
 	
-	IUI.makeUI();
+	IUI.makeUI();	
+	
+	var animateStyle = function(element, style, value, time, callback){
+		var obj={}
+			obj[style]=value;
+			
+		element.css(style,element.css(style)).animate(obj,time || 500, callback);
+	}
 	
 	
+	var _animateOutOfHome= function(){
+		
+		animateStyle($('.backdrop'),'height', '4rem');
+		setTimeout(function(){
+			animateStyle($('.header-big-text'),'margin-left', '0', 250);
+		
+		}, 400)
+		animateStyle($('.home-exhibit'),'height', '4rem');
+		animateStyle($('.header'),'top', '1rem');
+		animateStyle($('.header'),'top', '1rem');
+		$('#front-section').css({'overflow':'hidden', 'white-space': 'nowrap'});
+		
+		animateStyle($('#front-section'),'margin-left', '-100%',300);
+		animateStyle($('#front-section').find('.section-item').eq(1),'margin-right', '100%',300);
+		setTimeout(function(){
+				animateStyle($('.front-text'),'opacity', '0',200, function(){
+					$('.front-text').remove();
+				});
+				$('#content-exhibit').css('display','block');
+				debugger;
+				animateStyle($('#content-exhibit'),'height', $(window).outerHeight()- 4*parseInt($('body').css('font-size')) ,400)
+				
+		}, 100);
+	}
 	
-	
-	
-	
-	
-	
+	$('.front-text').on('click','.section-item', function(){
+		_animateOutOfHome()
+	})
 	
 	
 
